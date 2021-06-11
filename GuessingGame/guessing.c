ded4c72c464e082c16960ad0feacc6846d110042
Everton Brzozowy Alves <everton.brzozowy.alves@gmail.com>
2021-06-10 23:58:44 -0300

    created a for

--- GuessingGame/guessing.c
+++ GuessingGame/guessing.c
@@ -10,30 +10,37 @@ int main()
     int secretNumber = 42;
 
     // printf("The secret number is %d, keep it secret!\n", secretNumber);
-
+    int chances = 3;
     int guessNumber;
 
-    printf("Guess a number:");
-    scanf("%d", &guessNumber);
-    printf("Your thinking on number %d. Let's see... \n", guessNumber);
-
-    int guessed = (guessNumber == secretNumber); //0 => false, 1 => true
-
-    if (guessed)
+    for (int i = 1; i <= chances; i++)
     {
-        printf("You got it right! Congratulations. You won the game!!\n");
-        printf("Play again, you're a good player!!\n");
-    }
-    else
-    {
-        int guessIsHigherThanSecretNumber = guessNumber > secretNumber;
-        if (guessIsHigherThanSecretNumber)
+        printf("%d of %d tries\n", i, chances);
+        printf("Guess a number:");
+        scanf("%d", &guessNumber);
+        printf("Your thinking on number %d. Let's see... \n", guessNumber);
+
+        int guessed = (guessNumber == secretNumber); //0 => false, 1 => true
+
+        if (guessed)
         {
-            printf("Your guess is higher than the secret number!\n");
+            printf("You got it right! Congratulations. You won the game!!\n");
+            printf("Play again, you're a good player!!\n");
+            break;
         }
         else
         {
-            printf("Your guess is lower than the secret number!\n");
+            int guessIsHigherThanSecretNumber = guessNumber > secretNumber;
+            if (guessIsHigherThanSecretNumber)
+            {
+                printf("Your guess is higher than the secret number!\n");
+            }
+            else
+            {
+                printf("Your guess is lower than the secret number!\n");
+            }
         }
+        printf("\n");
     }
+    printf("Game over!\n");
 }
