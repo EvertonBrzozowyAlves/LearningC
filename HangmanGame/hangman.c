e51a6a5c75d24d43c4fa45d76d381e7f2c72fe83
Everton Brzozowy Alves <everton.brzozowy.alves@gmail.com>
2021-07-03 22:00:39 -0300

    started the hangman game

---
+++ HangmanGame/hangman.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    char secretWord[20];
+    sprintf(secretWord, "WATERMELON");
+
+    // secretWord[10] = '\0';
+    // printf("%s\n", secretWord);
+
+    int guessed = 0;
+    int hanged = 0;
+
+    do
+    {
+
+    } while (!guessed && !hanged);
+}
