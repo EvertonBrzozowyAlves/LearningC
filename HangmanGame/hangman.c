607deeea2f15bc2ec735fe813c53708108d04845
Everton Brzozowy Alves <everton.brzozowy.alves@gmail.com>
2021-07-06 21:15:35 -0300

    looping through guesses and tries

--- HangmanGame/hangman.c
+++ HangmanGame/hangman.c
@@ -19,14 +19,33 @@ int main()
     {
         for (int i = 0; i < strlen(secretWord); i++)
         {
-            printf("_ ");
+            int found = 0;
+            for (int j = 0; j < tries; j++)
+            {
+                if (guesses[j] == secretWord[i])
+                {
+                    found = 1;
+                    break;
+                }
+            }
+            if (found)
+            {
+                printf("%c ", secretWord[i]);
+            }
+            else
+            {
+                printf("_ ");
+            }
         }
         printf("\n");
+
+        printf("Guess a letter:\n");
 
         char letter;
         scanf(" %c", &letter);
 
         guesses[tries] = letter;
+        tries++;
 
     } while (!guessed && !hanged);
 }
